0465456804317828048243fc63dcc1c1a7488a06
alfred <alfred@DESKTOP-BVSRMUS.lan>
2018-01-19 21:53:11 +0800

    自定义shiro密码验证

---
+++ cloudstudy/src/main/java/com/cloudstudy/constant/Constant.java
@@ -0,0 +1,47 @@
+/******************************************************************
+ *    
+ *    Package:     com.cloudstudy.constant
+ *
+ *    Filename:    package-info.java
+ *
+ *    @author:     alfred
+ *
+ *    @version:    1.0.0
+ *
+ *    Create at:   2018年1月16日 下午5:35:56
+ *
+ *    - first revision 
+ *
+ *****************************************************************/
+/**
+ * 
+ * 
+ * @ClassName package-info 
+ * @author alfred 
+ * @Date 2018年1月16日 下午5:35:56 
+ * @version 1.0.0  
+ */
+package com.cloudstudy.constant;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource("classpath:config.properties")
+public class Constant {
+
+	@Value("${cloudstudy.project.name}")
+	private String projectName;
+	@Value("${cloudstudy.project.version}")
+	private String projectVersion;
+
+	public String getProjectName() {
+		return projectName;
+	}
+
+	public String getProjectVersion() {
+		return projectVersion;
+	}
+
+}
--- cloudstudy/src/main/java/com/cloudstudy/shiro/ShiroConfig.java
+++ cloudstudy/src/main/java/com/cloudstudy/shiro/ShiroConfig.java
@@ -5,9 +5,12 @@ import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
+
+import com.cloudstudy.constant.Constant;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -15,6 +18,9 @@ import java.util.Properties;
 
 @Configuration
 public class ShiroConfig {
+	@Autowired
+	private Constant constant;
+
 	@Bean
 	public ShiroFilterFactoryBean shirFilter(SecurityManager securityManager) {
 		System.out.println("ShiroConfiguration.shirFilter()");
@@ -26,9 +32,10 @@ public class ShiroConfig {
 		filterChainDefinitionMap.put("/static/**", "anon");
 		// 配置退出 过滤器,其中的具体的退出代码Shiro已经替我们实现了
 		filterChainDefinitionMap.put("/logout", "logout");
-		// <!-- 过滤链定义，从上向下顺序执行，一般将/**放在最为下边 -->:这是一个坑呢，一不小心代码就不好使了;
-		// <!-- authc:所有url都必须认证通过才可以访问; anon:所有url都都可以匿名访问-->
-		filterChainDefinitionMap.put("/**", "authc");
+		// 过滤链定义，从上向下顺序执行，一般将/**放在最为下边 ,这是一个坑呢，一不小心代码就不好使了;
+		// authc:所有url都必须认证通过才可以访问;
+		// anon:所有url都都可以匿名访问
+		filterChainDefinitionMap.put("/" + constant.getProjectName() + "/**", "authc");
 		// 如果不设置默认会自动寻找Web工程根目录下的"/login.jsp"页面
 		shiroFilterFactoryBean.setLoginUrl("/login");
 		// 登录成功后要跳转的链接
